9e8f7a96ed3722e283a58660088ac2920256fb49
ndcuong <ndcuong869@gmail.com>
2021-10-23 19:14:27 +0700

    Fix bug

--- app.py
+++ app.py
@@ -314,4 +314,4 @@ def updateSkillHandler():
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
